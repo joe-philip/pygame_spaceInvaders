8f9a85b5fa2d468baade336d51f848d958ebc310
Joe Philip <joe.philip@hotmail.co.in>
2021-02-19 20:21:26 +0530

    Set game caption

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
 pygame.init()  # Initialize pygame
 window = pygame.display.set_mode((800, 600))  # create game window
+pygame.display.set_caption("Game Caption")  # Setting game caption
 gameRunning = True
 while gameRunning:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:  # when close button is pressed
             gameRunning = False
