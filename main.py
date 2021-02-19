4fa9446de3587d299c2bf2be455af305cbbe92b2
Joe Philip <joe.philip@hotmail.co.in>
2021-02-19 20:16:50 +0530

    Added while loop for quiting game only when QUIT event occurs

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import pygame
 pygame.init()  # Initialize pygame
-window = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((800, 600))  # create game window
+gameRunning = True
+while gameRunning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameRunning = False
