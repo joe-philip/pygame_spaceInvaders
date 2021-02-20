0d0a5a6792b0b77931b7e2474a11d08b8b48551f
Joe Philip <joe.philip@hotmail.co.in>
2021-02-20 14:08:55 +0530

    Added code for window background fill

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import pygame
+
 pygame.init()  # Initialize pygame
+
 window = pygame.display.set_mode((800, 600))  # create game window
+
 pygame.display.set_caption("Space Invaders")  # Setting game caption
+
 Icon = pygame.image.load("Images/Icon.png")  # Setting Icon for app
 pygame.display.set_icon(Icon)  # Loading icon to app
+
 gameRunning = True
 while gameRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # when close button is pressed
             gameRunning = False
+
+    window.fill((0, 0, 0))  # Setting window background color
+    pygame.display.update()  # updated window to background color
