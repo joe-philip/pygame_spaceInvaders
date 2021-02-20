c13ceaec046b2e7d2814f5544de06b2180af62a8
Joe Philip <joe.philip@hotmail.co.in>
2021-02-20 19:47:49 +0530

    Added player spaceship to the game window

--- main.py
+++ main.py
@@ -6,8 +6,20 @@ window = pygame.display.set_mode((800, 600))  # create game window
 
 pygame.display.set_caption("Space Invaders")  # Setting game caption
 
-Icon = pygame.image.load("Images/Icon.png")  # Setting Icon for app
+# Setting Icon for app always 16x16 size images
+Icon = pygame.image.load("Images/Icon.png")
 pygame.display.set_icon(Icon)  # Loading icon to app
+
+# loading player image
+player_spaceship = pygame.image.load("Images/player.png")
+playerX = 370  # X position of the image on the screen
+playerY = 480  # Y position of the image on the screen
+
+
+def player(playerX, playerY):
+    # the blit function is used to draw objects to the game window
+    window.blit(player_spaceship, (playerX, playerY))
+
 
 gameRunning = True
 while gameRunning:
@@ -16,4 +28,5 @@ while gameRunning:
             gameRunning = False
 
     window.fill((0, 0, 0))  # Setting window background color
+    player(playerX, playerY)
     pygame.display.update()  # updated window to background color
