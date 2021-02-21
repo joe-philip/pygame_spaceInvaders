3a8ee7d60849ba6903da0a9ea60599e7c40aa593
Joe Philip <joe.philip@hotmail.co.in>
2021-02-21 17:37:08 +0530

    Moved spaceship according to keypress

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ pygame.display.set_icon(Icon)  # Loading icon to app
 player_spaceship = pygame.image.load("Images/player.png")
 playerX = 370  # X position of the image on the screen
 playerY = 480  # Y position of the image on the screen
+playerX_change = 0
+playerY_change = 0
 
 
 def player(playerX, playerY):
@@ -27,6 +29,22 @@ while gameRunning:
         if event.type == pygame.QUIT:  # when close button is pressed
             gameRunning = False
 
+        if event.type == pygame.KEYDOWN:  # true when any key is pressed
+            if event.key == pygame.K_LEFT:
+                playerX_change = -1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 1
+            if event.key == pygame.K_UP:
+                playerY_change = -1
+            if event.key == pygame.K_DOWN:
+                playerY_change = 1
+        if event.type == pygame.KEYUP:  # true when any key is released
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                playerY_change = 0
     window.fill((0, 0, 0))  # Setting window background color
+    playerX += playerX_change
+    playerY += playerY_change
     player(playerX, playerY)
     pygame.display.update()  # updated window to background color
